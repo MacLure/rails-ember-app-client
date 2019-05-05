130e0bf70478f3def679435f829a72e75e6af8fc
Malcolm <malcolm.maclure@gmail.com>
2019-05-04 22:57:24 -0400

    delete button, updates db

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -3,5 +3,13 @@ import Controller from "@ember/controller";
 export default Controller.extend({
   model() {
     return this.store.findAll("task");
+  },
+  actions: {
+    deleteTask: function(id) {
+      this.store.findRecord("task", id).then(function(task) {
+        task.deleteRecord();
+        task.save();
+      });
+    }
   }
 });
